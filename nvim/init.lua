976bace6a6eab2f843c3a4fd122bce569d3279a9
dB <smithwebdd@gmail.com>
2021-06-30 13:56:48 -0400

    updated plugins with deoplete, removed compe

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,2 @@
---if package.loaded['smithwebdev.start'] then
---    -- Useful for reloading namespaced configuration, but only
---    -- do this if it _needs_ to be reloaded
---    for pack, _ in pairs(package.loaded) do
---        if string.find(pack, '^smithwebdev%..') then
---            package.loaded[pack] = nil
---        end
---    end
---end
-
--- Global namespace
---_G.smithwebdev = {}
-
 require 'smithwebdev.start'
 require 'smithwebdev.plugins'
